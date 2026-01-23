f34e4091ee17b52ceb31ec37bfac6b85b414ef6e
ketanitsolution <67502541+ketanitsolution@users.noreply.github.com>
2026-01-23 23:04:25 +0530

    fix: simplify next.config.js standalone output

--- apps/web-portal/next.config.js
+++ apps/web-portal/next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ["@ghar-switch/domain-types", "@ghar-switch/constants"],
     output: 'standalone',
-    // Moved to root level as experimental.outputFileTracingRoot is deprecated
-    outputFileTracingRoot: path.join(__dirname, '../../'),
 };
 
 module.exports = nextConfig;
