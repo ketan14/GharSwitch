933199b5ee32e080fd49f9621a1461af33ad4133
ketanitsolution <67502541+ketanitsolution@users.noreply.github.com>
2026-01-23 22:48:32 +0530

    fix: move outputFileTracingRoot to top level config

--- apps/web-portal/next.config.js
+++ apps/web-portal/next.config.js
@@ -5,10 +5,8 @@ const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ["@ghar-switch/domain-types", "@ghar-switch/constants"],
     output: 'standalone',
-    experimental: {
-        // Required for monorepos to trace dependencies correctly
-        outputFileTracingRoot: path.join(__dirname, '../../'),
-    },
+    // Moved to root level as experimental.outputFileTracingRoot is deprecated
+    outputFileTracingRoot: path.join(__dirname, '../../'),
 };
 
 module.exports = nextConfig;
